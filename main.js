ffa93e6f2772d738600fe1f42bc9d4e20ef71722
MarcoLepDev <88764817+marcolepdev@users.noreply.github.com>
2024-05-26 19:06:46 +0200

    adding files

---
+++ main.js
@@ -0,0 +1,75 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            el: '#app',
+            addedTask: '',
+            tasks: [],
+            warningText: '',
+            maxLength: 40
+
+        }
+    },
+    computed: {
+        taskCount() {
+            return this.tasks.length;
+        }
+    },
+    methods: {
+        addTask() {
+            const trimmedText = this.addedTask.trim();
+            const whiteSpaces = this.addedTask.match(/^\s+/);
+
+            if (this.addedTask === '') {
+                this.warningText = 'Field is empty, please type something...';
+            } else {
+                if (whiteSpaces && whiteSpaces[0].length >= 1) {
+                    this.warningText = 'No white spaces allowed at the beginning. Please try again';
+                    this.addedTask = '';
+                } else {
+                    this.tasks.push(trimmedText);
+                    this.addedTask = '';
+                    this.warningText = ''
+
+                }
+
+            }
+
+        },
+        maxLengthInput(event) {
+            if (event.target.value.length === this.maxLength) {
+                this.warningText = 'text is too long';
+                this.event.target.value.slice(0, index);
+                
+            }else{
+                this.warningText=''
+            }
+        },
+        taskEnter() {
+            const trimmedText = this.addedTask.trim();
+            const whiteSpaces = this.addedTask.match(/^\s+/);
+
+            if (this.addedTask === '') {
+                this.warningText = 'Field is empty, please type something...';
+
+            } else {
+                if (whiteSpaces && whiteSpaces[0].length > 1) {
+                    this.warningText = 'too many white spaces at the beginning. Please type a proper word';
+                    this.addedTask = '';
+                } else {
+                    this.tasks.push(trimmedText);
+                    this.addedTask = '';
+                    this.warningText = ''
+                }
+
+            }
+
+        },
+        removeTask(index) {
+            this.tasks.splice(index, 1);
+        }
+    }
+}
+);
+
+
+app.mount('#app');
